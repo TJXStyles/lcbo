c46d78f4bb0db74874698212628dfad73eac9b3e
Jack Cooc <jack@users-MacBook-Pro.local>
2013-06-17 02:01:01 -0400

    html tags inserted

--- lcbo_generator.rb
+++ lcbo_generator.rb
@@ -1,16 +1,22 @@
 require 'json'
 require 'open-uri'
 
-raw_data = open("http://lcboapi.com/products/").read
-# puts raw_data
+		raw_data = open("http://lcboapi.com/products/").read
+		lcbo_data = JSON.parse(raw_data)
+		lcbo_data["result"].each do |product_hash|
 
-lcbo_data = JSON.parse(raw_data)
+		puts "<html>"
+	    puts "  <head>"
+	    puts "    <title>Connoisseur</title>"
+	    puts "    <link rel='stylesheet' href='normalize.css' type='text/css'>"
+	    puts "    <link rel='stylesheet' href='styles.css' type='text/css'>"
+	    puts "  </head>"
+	    puts "  <body>"
 
-# puts lcbo_data.class
-
-lcbo_data["result"].each do |product_hash|
-	puts "\nName: #{product_hash["name"]}"
-	puts "Type: #{product_hash["primary_category"]}"
-	puts "Package: #{product_hash["package"]}"
-	puts "Price: #{product_hash["regular_price_in_cents"].to_f/100}\n"
-end
+		puts "<p> Name: #{product_hash["name"]} </p>"
+		puts "<p>Type: #{product_hash["primary_category"]}</p>"
+		puts "<p>Package: #{product_hash["package"]}</p>"
+		puts "<p>Price: #{product_hash["regular_price_in_cents"].to_f/100}</p>"
+		puts "</body>"
+		puts "</html>"
+		end
